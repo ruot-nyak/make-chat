9f8e3f75d0b3008a22339f0e1f5d904a6704b8fd
Ruot <ruot@iMac.local>
2021-10-08 01:19:47 -0700

    Initial new channel

--- public/sockets/chat.js
+++ public/sockets/chat.js
@@ -1,5 +1,9 @@
 module.exports = (io, socket, onlineUsers) => {
 
+    socket.on('new channel', (newChannel) => {
+        console.log(newChannel);
+    });
+
     // Listen for "new user" socket emits
     socket.on('new user', (username) => {
         onlineUsers[username] = socket.id;
@@ -7,18 +11,18 @@ module.exports = (io, socket, onlineUsers) => {
         console.log(`${username} has joined the chat! ✋`);
         //Send the username to all clients currently connected
         io.emit("new user", username);
-    })
+    });
 
     socket.on('new message', (data) => {
         console.log(`${data.sender} just posted a new message!`);
         //posting the message to all users
         io.emit('new message', data);
-    })
+    });
 
     socket.on('get online users', () => {
         //Send over the onlineUsers
         socket.emit('get online users', onlineUsers);
-    })
+    });
 
     socket.on('disconnect', () => {
         //This deletes the user by using the username we saved to the socket
